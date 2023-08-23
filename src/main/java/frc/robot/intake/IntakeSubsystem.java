cbcd745272fbf18cc34f09cc53fab00ccf5e85bb
Owen <117956892+Owen756@users.noreply.github.com>
2023-08-22 17:27:46 -0700

    added motor controls

--- src/main/java/frc/robot/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/intake/IntakeSubsystem.java
@@ -11,17 +11,29 @@ import frc.robot.util.scheduling.SubsystemPriority;
 /** Add your docs here. */
 public class IntakeSubsystem extends LifecycleSubsystem {
   private CANSparkMax motor;
-  public IntakeState state;
+  private IntakeState state;
+
   public IntakeSubsystem(CANSparkMax motor) {
     super(SubsystemPriority.INTAKE);
     this.motor = motor;
   }
-  public void setState(IntakeState intakeState){
+
+  public void setState(IntakeState intakeState) {
     state = intakeState;
   }
 
   @Override
   public void enabledPeriodic() {
-
+    if (state == IntakeState.INTAKE_CUBE) {
+      motor.set(0.5);
+    } else if (state == IntakeState.INTAKE_CONE) {
+      motor.set(-1);
+    } else if (state == IntakeState.OUTTAKE_CONE) {
+      motor.set(0.4);
+    } else if (state == IntakeState.OUTTAKE_CUBE) {
+      motor.set(-0.3);
+    } else {
+      motor.set(0.0);
+    }
   }
 }
