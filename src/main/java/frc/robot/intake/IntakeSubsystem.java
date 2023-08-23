5f01aecccc0e00120c1384e4139cb35849ff04d3
Owen <117956892+Owen756@users.noreply.github.com>
2023-08-22 18:58:28 -0700

    initial intake commit (#7)
    * initial intake commit
    
    * added intake state
    
    * added motor controls
    
    * added held game piece
    
    * added goal and better intake states
    
    * Format with Spotless
    
    ---------
    
    Co-authored-by: Jordan <117786700+jordanjcoderman@users.noreply.github.com>

--- src/main/java/frc/robot/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/intake/IntakeSubsystem.java
@@ -11,14 +11,72 @@ import frc.robot.util.scheduling.SubsystemPriority;
 /** Add your docs here. */
 public class IntakeSubsystem extends LifecycleSubsystem {
   private CANSparkMax motor;
+  private IntakeState goalState;
+  private HeldGamePiece gamePiece = HeldGamePiece.NOTHING;
 
   public IntakeSubsystem(CANSparkMax motor) {
     super(SubsystemPriority.INTAKE);
     this.motor = motor;
   }
 
+  public void setGoalState(IntakeState intakeState) {
+    if (goalState != intakeState) {
+      if (goalState == IntakeState.INTAKE_CONE || goalState == IntakeState.INTAKE_CUBE) {
+        gamePiece = HeldGamePiece.NOTHING;
+      }
+    }
+
+    goalState = intakeState;
+  }
+
   @Override
   public void enabledPeriodic() {
-    
+    if (goalState == IntakeState.OUTTAKE_CONE) {
+      motor.set(0.4);
+      // gamePiece = HeldGamePiece.NOTHING;
+    } else if (goalState == IntakeState.OUTTAKE_CUBE) {
+      motor.set(-0.3);
+      // gamePiece = HeldGamePiece.NOTHING;
+    } else if (gamePiece == HeldGamePiece.CUBE) {
+      motor.set(0.15);
+    } else if (gamePiece == HeldGamePiece.CONE) {
+      motor.set(-0.1);
+    } else if (goalState == IntakeState.INTAKE_CUBE) {
+      motor.set(0.5);
+      // gamePiece = HeldGamePiece.CUBE;
+    } else if (goalState == IntakeState.INTAKE_CONE) {
+      motor.set(-1);
+      // gamePiece = HeldGamePiece.CONE;
+    } else {
+      motor.disable();
+    }
+  }
+
+  // create an atGoal(IntakeMode mode) method
+  public boolean atGoal(IntakeState state) {
+    if (goalState != state) {
+      return false;
+    }
+    if (state == IntakeState.OUTTAKE_CONE || state == IntakeState.OUTTAKE_CUBE) {
+      return gamePiece == HeldGamePiece.NOTHING;
+    }
+    if (state == IntakeState.STOPPED) {
+      return true;
+    }
+    if (state == IntakeState.INTAKE_CUBE) {
+      return gamePiece == HeldGamePiece.CUBE;
+    }
+    if (state == IntakeState.INTAKE_CONE) {
+      return gamePiece == HeldGamePiece.CONE;
+    }
+    return false;
+  }
+
+  public HeldGamePiece getGamePiece() {
+    return gamePiece;
+  }
+
+  public void setGamePiece(HeldGamePiece gamePiece) {
+    this.gamePiece = gamePiece;
   }
 }
