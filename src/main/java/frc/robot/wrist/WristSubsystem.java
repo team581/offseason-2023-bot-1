8f4bbd4d800afe59074364c0f82e18adcd4215a3
Owen <117956892+Owen756@users.noreply.github.com>
2023-08-22 18:57:34 -0700

    wrist code completed except homing

--- src/main/java/frc/robot/wrist/WristSubsystem.java
+++ src/main/java/frc/robot/wrist/WristSubsystem.java
@@ -1,55 +1,63 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.wrist;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.util.scheduling.LifecycleSubsystem;
 import frc.robot.util.scheduling.SubsystemPriority;
-import frc.robot.wrist.WristSubsystem;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import edu.wpi.first.wpilibj2.command.Command;
 
 public class WristSubsystem extends LifecycleSubsystem {
-  private CANSparkMax motor = new CANSparkMax(1, MotorType.kBrushless);
-  private boolean zeroed = false;
-  private boolean active = false;
-  private RelativeEncoder encoder = motor.getEncoder();
-  private double goalAngle = 0;
+  private final CANSparkMax motor;
+  private RelativeEncoder encoder;
+  private SparkMaxPIDController pid;
+  private double goalAngle = 0.0;
 
   public WristSubsystem(CANSparkMax motor) {
     super(SubsystemPriority.WRIST);
 
     this.motor = motor;
-  }
-  @Override
-  public void enabledPeriodic() {
-    if (active && zeroed) {
-      encoder.setPosition(goalAngle / 360.0);
-    } else {
-      motor.disable();
-    }
+    encoder = motor.getEncoder();
+    pid = motor.getPIDController();
+
+    pid.setP(5);
+    pid.setI(0);
+    pid.setD(0);
+
+    encoder.setPosition(0);
+    encoder.setPositionConversionFactor(50);
   }
 
-  public Command setZero() {
-    return runOnce(() -> {
-      zeroed = true;
-    });
+  @Override
+  public void enabledPeriodic() {
+    pid.setReference(goalAngle / 360.0, ControlType.kSmartMotion);
   }
-  public Command setDisabledCommand() {
-    return runOnce(() -> {
-      active = false;
-    });
+
+  public double set(double angle) {
+    goalAngle = angle;
+    return goalAngle;
   }
+
+// create a method set(double angle), which sets the goal angle
+  // update setPositionCommand to use the set() method
+
   public Command setPositionCommand(double angle) {
     return run(() -> {
-      goalAngle = angle;
-      active = true;
-    })
-    .until(() -> atAngle(angle));
+          set(angle);
+        })
+        .until(() -> atAngle(angle));
   }
+
   private boolean atAngle(double angle) {
     return Math.abs(getWristAngle() - angle) < 1;
   }
+
   private double getWristAngle() {
     return encoder.getPosition() * 360.0;
   }
 }
-
