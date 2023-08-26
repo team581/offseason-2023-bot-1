d727f83fd39e96ccba88c6c8f64264aabe015bd7
Owen <117956892+Owen756@users.noreply.github.com>
2023-08-26 10:50:23 -0700

    change from double to rotation2d in wrist

--- src/main/java/frc/robot/wrist/WristSubsystem.java
+++ src/main/java/frc/robot/wrist/WristSubsystem.java
@@ -3,6 +3,8 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
+
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.util.scheduling.LifecycleSubsystem;
 import frc.robot.util.scheduling.SubsystemPriority;
@@ -11,7 +13,7 @@ public class WristSubsystem extends LifecycleSubsystem {
   private final CANSparkMax motor;
   private RelativeEncoder encoder;
   private SparkMaxPIDController pid;
-  private double goalAngle = 0.0;
+  private Rotation2d goalAngle = new Rotation2d();
 
   public WristSubsystem(CANSparkMax motor) {
     super(SubsystemPriority.WRIST);
@@ -30,25 +32,26 @@ public class WristSubsystem extends LifecycleSubsystem {
 
   @Override
   public void enabledPeriodic() {
-    pid.setReference(goalAngle / 360.0, ControlType.kSmartMotion);
+    pid.setReference(goalAngle.getRotations(), ControlType.kSmartMotion);
   }
 
-  public void set(double angle) {
+  public void set(Rotation2d angle) {
     goalAngle = angle;
   }
 
-  public Command setPositionCommand(double angle) {
+  public Command setPositionCommand(Rotation2d angle) {
     return run(() -> {
           set(angle);
         })
         .until(() -> atAngle(angle));
   }
 
-  private boolean atAngle(double angle) {
-    return Math.abs(getWristAngle() - angle) < 1;
+  private boolean atAngle(Rotation2d angle) {
+    double actualAngle = getWristAngle().getDegrees();
+    return Math.abs(actualAngle - angle.getDegrees()) < 1;
   }
 
-  private double getWristAngle() {
-    return encoder.getPosition() * 360.0;
+  private Rotation2d getWristAngle() {
+    return Rotation2d.fromRotations(encoder.getPosition());
   }
 }
