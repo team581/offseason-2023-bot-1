6204bf707cdbd7a66eb7ad02e418703123242e72
Owen <117956892+Owen756@users.noreply.github.com>
2023-08-26 11:49:15 -0700

    Add intake velocity gamepiece detection Closes #10

--- src/main/java/frc/robot/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/intake/IntakeSubsystem.java
@@ -5,19 +5,26 @@
 package frc.robot.intake;
 
 import com.revrobotics.*;
+
+import edu.wpi.first.math.filter.LinearFilter;
 import frc.robot.util.scheduling.LifecycleSubsystem;
 import frc.robot.util.scheduling.SubsystemPriority;
 import org.littletonrobotics.junction.Logger;
 
 /** Add your docs here. */
 public class IntakeSubsystem extends LifecycleSubsystem {
-  private CANSparkMax motor;
+  private final LinearFilter voltageFilter = LinearFilter.movingAverage(7);
+  private final LinearFilter velocityFilter = LinearFilter.movingAverage(7);
+  //7 is a placeholder not tuned
+  private final CANSparkMax motor;
+  private final RelativeEncoder encoder;
   private IntakeState goalState;
   private HeldGamePiece gamePiece = HeldGamePiece.NOTHING;
-
+  
   public IntakeSubsystem(CANSparkMax motor) {
     super(SubsystemPriority.INTAKE);
     this.motor = motor;
+    encoder = motor.getEncoder();
   }
 
   public void setGoalState(IntakeState intakeState) {
@@ -42,26 +49,34 @@ public class IntakeSubsystem extends LifecycleSubsystem {
   public void enabledPeriodic() {
     if (goalState == IntakeState.OUTTAKE_CONE) {
       motor.set(0.4);
-      // gamePiece = HeldGamePiece.NOTHING;
     } else if (goalState == IntakeState.OUTTAKE_CUBE) {
       motor.set(-0.3);
-      // gamePiece = HeldGamePiece.NOTHING;
     } else if (gamePiece == HeldGamePiece.CUBE) {
       motor.set(0.15);
     } else if (gamePiece == HeldGamePiece.CONE) {
       motor.set(-0.1);
     } else if (goalState == IntakeState.INTAKE_CUBE) {
       motor.set(0.5);
-      // gamePiece = HeldGamePiece.CUBE;
     } else if (goalState == IntakeState.INTAKE_CONE) {
       motor.set(-1);
-      // gamePiece = HeldGamePiece.CONE;
     } else {
       motor.disable();
     }
+
+    // Game piece detection
+    double motorVelocity = velocityFilter.calculate(encoder.getVelocity());
+    double intakeVoltage = voltageFilter.calculate(motor.getAppliedOutput()) * 12.0;
+    double theoreticalSpeed = intakeVoltage * (5700.0/12.0); //Neo Max is 5700
+    double threshold = theoreticalSpeed * 0.5; 
+    if (motorVelocity < threshold && goalState == IntakeState.INTAKE_CONE) {
+      gamePiece = HeldGamePiece.CONE;
+    } else if (motorVelocity < threshold && goalState == IntakeState.INTAKE_CUBE) {
+      gamePiece = HeldGamePiece.CUBE;
+    } else if (motorVelocity > threshold && (goalState == IntakeState.OUTTAKE_CONE || goalState == IntakeState.OUTTAKE_CUBE)) {
+      gamePiece = HeldGamePiece.NOTHING;
+    }
   }
 
-  // create an atGoal(IntakeMode mode) method
   public boolean atGoal(IntakeState state) {
     if (goalState != state) {
       return false;
