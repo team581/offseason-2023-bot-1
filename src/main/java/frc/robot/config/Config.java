ad0cba313b5d84d4e06332c47680c02175d054cf
Owen <117956892+Owen756@users.noreply.github.com>
2023-08-29 18:19:56 -0700

    port over swerve from spike hector & susan

--- src/main/java/frc/robot/config/Config.java
+++ src/main/java/frc/robot/config/Config.java
@@ -4,6 +4,14 @@
 
 package frc.robot.config;
 
+import com.pathplanner.lib.auto.PIDConstants;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import frc.robot.localization.VisionMode;
+import frc.robot.swerve.SwerveCorner;
+import frc.robot.swerve.SwerveModuleConstants;
+
 public class Config {
   public static final String SERIAL_NUMBER = System.getenv("serialnum");
 
@@ -12,5 +20,79 @@ public class Config {
 
   public static final String CANIVORE_ID = "581CANivore";
 
+  public static final VisionMode VISION_MODE = VisionMode.ENABLED_UNUSED;
+public static final Translation2d SWERVE_FRONT_LEFT_LOCATION =
+   new Translation2d(0.263525, 0.263525);
+public static final Translation2d SWERVE_FRONT_RIGHT_LOCATION =
+   new Translation2d(0.263525, -0.263525);
+public static final Translation2d SWERVE_BACK_LEFT_LOCATION =
+   new Translation2d(-0.263525, 0.263525);
+public static final Translation2d SWERVE_BACK_RIGHT_LOCATION =
+   new Translation2d(-0.263525, -0.263525);
+
+  public static final double WHEEL_DIAMETER = Units.inchesToMeters(3.84);
+  public static final double SWERVE_STEER_GEARING_REDUCTION = 150.0 / 7.0;
+  public static final double SWERVE_DRIVE_GEARING_REDUCTION =
+      50.0 * 16.0 * 45.0 / 14.0 / 28.0 / 15.0;
+  public static final double SWERVE_STEER_KV = 0.0;
+  public static final double SWERVE_STEER_KP = 5.0;
+  public static final double SWERVE_STEER_KI = 0.0;
+  public static final double SWERVE_STEER_KD = 0.0;
+  public static final double SWERVE_STEER_KS = 0.0;
+
+  public static final int SWERVE_DRIVE_VOLTAGE_PEAK_FORWARD_VOLTAGE = 12;
+  public static final int SWERVE_DRIVE_VOLTAGE_PEAK_REVERSE_VOLTAGE = -12;
+  public static final double SWERVE_DRIVE_CURRENT_LIMIT = 35.0;
+  public static final boolean SWERVE_DRIVE_LIMITS_ENABLE = true;
+
+  public static final double SWERVE_DRIVE_KP = 0.24;
+  public static final double SWERVE_DRIVE_KI = 0.0;
+  public static final double SWERVE_DRIVE_KD = 0.0;
+  public static final double SWERVE_DRIVE_KV = 0.1185;
+  public static final double SWERVE_DRIVE_KS = 0.0;
+
+  public static final double STEER_MOTOR_LIMITS = 35;
+  public static final boolean SWERVE_MOTOR_LIMITS_ENABLED = true;
+  public static final PIDConstants SWERVE_TRANSLATION_PID = new PIDConstants(2.5, 0, 0);
+  public static final PIDConstants SWERVE_ROTATION_PID = new PIDConstants(4.5, 0, 0.1);
+  public static final PIDConstants SWERVE_ROTATION_SNAP_PID = PIDConstants(7.5, 0, 0.5);
+  public static final boolean SWERVE_USE_FOC = true;
+
+  final int SWERVE_FL_DRIVE_MOTOR_ID = 8;
+  public static final int SWERVE_FL_STEER_MOTOR_ID = 9;
+  public static final int SWERVE_FL_CANCODER_ID = 13;
+  public static final SwerveModuleConstants SWERVE_FL_CONSTANTS =
+      new SwerveModuleConstants(
+          Rotation2d.fromDegrees(182.021484375), SwerveCorner.FRONT_LEFT, true, true);
+
+  // -62.84
+  public static final int SWERVE_FR_DRIVE_MOTOR_ID = 6;
+  public static final int SWERVE_FR_STEER_MOTOR_ID = 7;
+  public static final int SWERVE_FR_CANCODER_ID = 12;
+  public static final SwerveModuleConstants SWERVE_FR_CONSTANTS =
+      new SwerveModuleConstants(
+          Rotation2d.fromDegrees(159.521484375), SwerveCorner.FRONT_RIGHT, true, true);
+
+  // -147.8
+  public static final int SWERVE_BL_DRIVE_MOTOR_ID = 4;
+  public static final int SWERVE_BL_STEER_MOTOR_ID = 5;
+  public static final int SWERVE_BL_CANCODER_ID = 11;
+  public static final SwerveModuleConstants SWERVE_BL_CONSTANTS =
+      new SwerveModuleConstants(
+          Rotation2d.fromDegrees(2.548828125), SwerveCorner.BACK_LEFT, true, true);
+  // 78.75
+  public static final int SWERVE_BR_DRIVE_MOTOR_ID = 2;
+  public static final int SWERVE_BR_STEER_MOTOR_ID = 3;
+  public static final int SWERVE_BR_CANCODER_ID = 10;
+  public static final SwerveModuleConstants SWERVE_BR_CONSTANTS =
+      new SwerveModuleConstants(
+          Rotation2d.fromDegrees(110.390625), SwerveCorner.BACK_RIGHT, true, true);
+
+  // 104.58
+
   private Config() {}
+
+  private static PIDConstants PIDConstants(double d, int i, double e) {
+    return null;
+  }
 }
