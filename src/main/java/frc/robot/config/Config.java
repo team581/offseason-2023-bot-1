fb4d3018dbce9c70d66c3ef4697867c393f32a9f
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2023-10-31 17:20:41 -0700

    Fix swerve device IDs

--- src/main/java/frc/robot/config/Config.java
+++ src/main/java/frc/robot/config/Config.java
@@ -64,32 +64,32 @@ public class Config {
   public static final PIDConstants SWERVE_ROTATION_SNAP_PID = new PIDConstants(7.5, 0, 0.5);
   public static final boolean SWERVE_USE_FOC = true;
 
-  public static final int SWERVE_FL_DRIVE_MOTOR_ID = 8;
-  public static final int SWERVE_FL_STEER_MOTOR_ID = 9;
-  public static final int SWERVE_FL_CANCODER_ID = 13;
+  public static final int SWERVE_FL_DRIVE_MOTOR_ID = 2;
+  public static final int SWERVE_FL_STEER_MOTOR_ID = 3;
+  public static final int SWERVE_FL_CANCODER_ID = 10;
   public static final SwerveModuleConstants SWERVE_FL_CONSTANTS =
       new SwerveModuleConstants(
           Rotation2d.fromDegrees(182.021484375), SwerveCorner.FRONT_LEFT, true, true);
 
   // -62.84
-  public static final int SWERVE_FR_DRIVE_MOTOR_ID = 6;
-  public static final int SWERVE_FR_STEER_MOTOR_ID = 7;
-  public static final int SWERVE_FR_CANCODER_ID = 12;
+  public static final int SWERVE_FR_DRIVE_MOTOR_ID = 4;
+  public static final int SWERVE_FR_STEER_MOTOR_ID = 5;
+  public static final int SWERVE_FR_CANCODER_ID = 11;
   public static final SwerveModuleConstants SWERVE_FR_CONSTANTS =
       new SwerveModuleConstants(
           Rotation2d.fromDegrees(159.521484375), SwerveCorner.FRONT_RIGHT, true, true);
 
   // -147.8
-  public static final int SWERVE_BL_DRIVE_MOTOR_ID = 4;
-  public static final int SWERVE_BL_STEER_MOTOR_ID = 5;
-  public static final int SWERVE_BL_CANCODER_ID = 11;
+  public static final int SWERVE_BL_DRIVE_MOTOR_ID = 6;
+  public static final int SWERVE_BL_STEER_MOTOR_ID = 7;
+  public static final int SWERVE_BL_CANCODER_ID = 12;
   public static final SwerveModuleConstants SWERVE_BL_CONSTANTS =
       new SwerveModuleConstants(
           Rotation2d.fromDegrees(2.548828125), SwerveCorner.BACK_LEFT, true, true);
   // 78.75
-  public static final int SWERVE_BR_DRIVE_MOTOR_ID = 2;
-  public static final int SWERVE_BR_STEER_MOTOR_ID = 3;
-  public static final int SWERVE_BR_CANCODER_ID = 10;
+  public static final int SWERVE_BR_DRIVE_MOTOR_ID = 8;
+  public static final int SWERVE_BR_STEER_MOTOR_ID = 9;
+  public static final int SWERVE_BR_CANCODER_ID = 13;
   public static final SwerveModuleConstants SWERVE_BR_CONSTANTS =
       new SwerveModuleConstants(
           Rotation2d.fromDegrees(110.390625), SwerveCorner.BACK_RIGHT, true, true);
