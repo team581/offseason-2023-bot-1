092267f0e4f8edc75afb93fd2ed42f7fb7025c27
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2023-11-02 18:58:24 -0700

    Log command scheduler to NT

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -141,6 +142,8 @@ public class Robot extends LoggedRobot {
     configureButtonBindings();
 
     enableLiveWindowInTest(false);
+
+    SmartDashboard.putData(CommandScheduler.getInstance());
   }
 
   /**
