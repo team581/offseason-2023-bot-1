946aa5ba9d79a6f5fcc9431a59778673b82eb490
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2023-11-04 10:56:22 -0700

    owen started auto builder

--- src/main/java/frc/robot/autos/Autos.java
+++ src/main/java/frc/robot/autos/Autos.java
@@ -5,6 +5,7 @@
 package frc.robot.autos;
 
 import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.auto.SwerveAutoBuilder;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 import com.pathplanner.lib.server.PathPlannerServer;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -17,63 +18,127 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.config.Config;
 import frc.robot.fms.FmsSubsystem;
+import frc.robot.intake.IntakeSubsystem;
+import frc.robot.localization.LocalizationSubsystem;
+import frc.robot.swerve.SwerveSubsystem;
+import frc.robot.wrist.WristSubsystem;
 import java.lang.ref.WeakReference;
 import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.HashMap;
 import java.util.Map;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 public class Autos {
+  private static Command wrapAutoEvent(String commandName, Command command) {
+    return Commands.sequence(
+            Commands.print("[COMMANDS] Starting auto event " + commandName),
+            command.deadlineWith(
+                Commands.waitSeconds(5)
+                    .andThen(
+                        Commands.print(
+                            "[COMMANDS] Auto event "
+                                + commandName
+                                + " has been running for 5+ seconds!"))),
+            Commands.print("[COMMANDS] Finished auto event " + commandName))
+        .handleInterrupt(() -> System.out.println("[COMMANDS] Cancelled auto event " + commandName))
+        .withName(commandName);
+  }
+
+  private static Map<String, Command> wrapAutoEventMap(Map<String, Command> eventMap) {
+    Map<String, Command> wrappedMap = new HashMap<>();
+    for (Map.Entry<String, Command> entry : eventMap.entrySet()) {
+      wrappedMap.put(
+          entry.getKey(), wrapAutoEvent("AutoEvent_" + entry.getKey(), entry.getValue()));
+    }
+    return wrappedMap;
+  }
+
   private final LoggedDashboardChooser<AutoKindWithoutTeam> autoChooser =
       new LoggedDashboardChooser<>("Auto Choices");
   private final Map<AutoKind, WeakReference<Command>> autosCache = new EnumMap<>(AutoKind.class);
+  private final SwerveAutoBuilder autoBuilder;
+  private final LocalizationSubsystem localization;
+  private final SwerveSubsystem swerve;
+  private final IntakeSubsystem intake;
+  private final WristSubsystem wrist;
 
-  public Autos() {
-    CommandScheduler.getInstance()
-        .onCommandInitialize(
-            command -> System.out.println("[COMMANDS] Starting command " + command.getName()));
-    CommandScheduler.getInstance()
-        .onCommandInterrupt(
-            command -> System.out.println("[COMMANDS] Cancelled command " + command.getName()));
-    CommandScheduler.getInstance()
-        .onCommandFinish(
-            command -> System.out.println("[COMMANDS] Finished command " + command.getName()));
+  public Autos(
+      LocalizationSubsystem localization,
+      SwerveSubsystem swerve,
+      IntakeSubsystem intake,
+      WristSubsystem wrist) {
 
-    autoChooser.addOption("Do nothing", AutoKindWithoutTeam.DO_NOTHING);
+    this.localization = localization;
+    this.swerve = swerve;
+    this.intake = intake;
+    this.wrist = wrist;
+    Map<String, Command> eventMap = Map.ofEntries();
 
-    if (Config.IS_DEVELOPMENT) {
-      PathPlannerServer.startServer(5811);
-    }
+    eventMap = wrapAutoEventMap(eventMap);
 
-    Logger.getInstance().recordOutput("Autos/CurrentTrajectory", new Trajectory());
-    Logger.getInstance().recordOutput("Autos/TargetPose", new Pose2d());
-    Logger.getInstance().recordOutput("Autos/SetpointSpeeds/X", 0);
-    Logger.getInstance().recordOutput("Autos/SetpointSpeeds/Y", 0);
-    Logger.getInstance().recordOutput("Autos/SetpointSpeeds/Omega", 0);
-    Logger.getInstance().recordOutput("Autos/TranslationError", new Pose2d());
-    Logger.getInstance().recordOutput("Autos/RotationError", 0);
+      autoBuilder =
+          new SwerveAutoBuilder(
+              localization::getPose,
+              localization::resetPose,
+              SwerveSubsystem.KINEMATICS,
+              Config.SWERVE_TRANSLATION_PID,
+              Config.SWERVE_ROTATION_PID,
+              (states) -> swerve.setModuleStates(states, false, false),
+              eventMap,
+              false,
+              swerve);
 
-    PPSwerveControllerCommand.setLoggingCallbacks(
-        (PathPlannerTrajectory activeTrajectory) -> {
-          Logger.getInstance().recordOutput("Autos/CurrentTrajectory", activeTrajectory);
-        },
-        (Pose2d targetPose) -> {
-          Logger.getInstance().recordOutput("Autos/TargetPose", targetPose);
-        },
-        (ChassisSpeeds setpointSpeeds) -> {
-          Logger.getInstance()
-              .recordOutput("Autos/SetpointSpeeds/X", setpointSpeeds.vxMetersPerSecond);
-          Logger.getInstance()
-              .recordOutput("Autos/SetpointSpeeds/Y", setpointSpeeds.vyMetersPerSecond);
-          Logger.getInstance()
-              .recordOutput("Autos/SetpointSpeeds/Omega", setpointSpeeds.omegaRadiansPerSecond);
-        },
-        (Translation2d translationError, Rotation2d rotationError) -> {
-          Logger.getInstance()
-              .recordOutput(
-                  "Autos/TranslationError", new Pose2d(translationError, new Rotation2d()));
-          Logger.getInstance().recordOutput("Autos/RotationError", rotationError.getDegrees());
-        });
+      CommandScheduler.getInstance()
+          .onCommandInitialize(
+              command -> System.out.println("[COMMANDS] Starting command " + command.getName()));
+      CommandScheduler.getInstance()
+          .onCommandInterrupt(
+              command -> System.out.println("[COMMANDS] Cancelled command " + command.getName()));
+      CommandScheduler.getInstance()
+          .onCommandFinish(
+              command -> System.out.println("[COMMANDS] Finished command " + command.getName()));
+
+
+              for (AutoKindWithoutTeam autoKind : EnumSet.allOf(AutoKindWithoutTeam.class)) {
+                autoChooser.addOption(autoKind.toString(), autoKind);
+              }
+
+      if (Config.IS_DEVELOPMENT) {
+        PathPlannerServer.startServer(5811);
+      }
+
+      Logger.getInstance().recordOutput("Autos/CurrentTrajectory", new Trajectory());
+      Logger.getInstance().recordOutput("Autos/TargetPose", new Pose2d());
+      Logger.getInstance().recordOutput("Autos/SetpointSpeeds/X", 0);
+      Logger.getInstance().recordOutput("Autos/SetpointSpeeds/Y", 0);
+      Logger.getInstance().recordOutput("Autos/SetpointSpeeds/Omega", 0);
+      Logger.getInstance().recordOutput("Autos/TranslationError", new Pose2d());
+      Logger.getInstance().recordOutput("Autos/RotationError", 0);
+
+      PPSwerveControllerCommand.setLoggingCallbacks(
+          (PathPlannerTrajectory activeTrajectory) -> {
+            Logger.getInstance().recordOutput("Autos/CurrentTrajectory", activeTrajectory);
+          },
+          (Pose2d targetPose) -> {
+            Logger.getInstance().recordOutput("Autos/TargetPose", targetPose);
+          },
+          (ChassisSpeeds setpointSpeeds) -> {
+            Logger.getInstance()
+                .recordOutput("Autos/SetpointSpeeds/X", setpointSpeeds.vxMetersPerSecond);
+            Logger.getInstance()
+                .recordOutput("Autos/SetpointSpeeds/Y", setpointSpeeds.vyMetersPerSecond);
+            Logger.getInstance()
+                .recordOutput("Autos/SetpointSpeeds/Omega", setpointSpeeds.omegaRadiansPerSecond);
+          },
+          (Translation2d translationError, Rotation2d rotationError) -> {
+            Logger.getInstance()
+                .recordOutput(
+                    "Autos/TranslationError", new Pose2d(translationError, new Rotation2d()));
+            Logger.getInstance().recordOutput("Autos/RotationError", rotationError.getDegrees());
+          });
+
   }
 
   public Command getAutoCommand() {
@@ -89,6 +154,10 @@ public class Autos {
       return Commands.none();
     }
 
+    // right now, this function just returns null
+    // instead, it needs to actually return the command to run during auto
+    // i recommend you create a field in this class for a pathplanner swerve auto builder
+    // then, you can take that and do auto builder.fullAuto(path);
     return null;
   }
 
